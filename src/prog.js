f8df8e13790ff1c44dae3d52a19418317b3257fd
Tso, Peter <tsopeter@ku.edu>
2020-10-09 16:00:05 -0500

    added runner

--- src/prog.js
+++ src/prog.js
@@ -10,6 +10,15 @@ function runGame(){
 	
 	/* run async program here */
 	while(gameRunFlag){
-		
+		let runner = new Promise(playerside, enemyside);
+		runner.then(playerside).catch(enemyside);
 	};
+}
+
+function playerside(result){
+	
+}
+
+function enemyside(error){
+
 }
